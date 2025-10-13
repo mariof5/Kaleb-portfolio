1c9451563763b13d771e7698f19fe39b0807b9ad
Mario <mariosent@gmail.com>
2025-10-13 21:29:42 +0300

    portfolio path fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Header/Navbar/Navbar'
 import About from './components/About/About'
-import Portfolio from './components/Portfolio/portfolio'
+import Portfolio from './components/Portfolio/Portfolio'
 import './App.css';
 import Footer from './components/Footer/Footer'
 import Services from './components/Services/Services';
